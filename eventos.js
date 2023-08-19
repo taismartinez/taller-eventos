c6d22b91892d0c36e1c093e0e420b79ef0c44a18
taismartinez <tati06mar@gmail.com>
2023-08-19 17:31:22 -0300

    h

--- eventos.js
+++ eventos.js
@@ -1,14 +1,14 @@
 
 document.getElementById("divalert").addEventListener("click", divAlert)
+document.getElementById("btn").addEventListener("click", function(event){
+alert("Hola!")
+event.stopPropagation();
+})
+
 
 function divAlert(){
-
-
+    
     alert("Hola! Soy el div")
 }
 
-function saludar(){
 
-    alert("Hola!")
-
-}
